ce6ae8d07077b670734789ccaad3d4643add3f0f
Eimantas <eimantas.blazevicius@bazaarvoice.com>
2018-11-05 20:06:07 +0200

    Update PartOne/Module 3.2/Module3FinalTask.py

--- PartOne/Module 3.2/Module3FinalTask.py
+++ PartOne/Module 3.2/Module3FinalTask.py
@@ -1,11 +1,12 @@
 class stock:
-
+#Definig variables
     def __init__(self, var):
         self.fullStock =  737
         self.minimumOrder = 5
         self.unitPrice = 7.37
-        self.var = int(var)
+        self.var = int(var) #making value that is provided a integer
 
+#function to check and do actions
     def isInStock(self):
         if self.var > self.fullStock:
             print("You want too much my friend")
@@ -14,9 +15,9 @@ class stock:
         else:
             print("Yes, Ok, so " + str(self.var) + " kg of cheese would cost " + str(self.var * self.unitPrice) + ".")
 
-
+#take the amount of cheese that is needed
 print("Kiek nori surio zmogau?")
 kiekNori = input()
-
+#assign class to a variable and call the class function.
 var = stock(kiekNori)
 var.isInStock()
