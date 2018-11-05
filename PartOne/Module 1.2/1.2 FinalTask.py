de07be3190f340df356074a0ad7661f055e3e24a
Eimantas <eimantas.blazevicius@bazaarvoice.com>
2018-11-05 19:19:43 +0200

    final task of 1.2

---
+++ PartOne/Module 1.2/1.2 FinalTask.py
@@ -0,0 +1,30 @@
+# Create Allergy check code
+# then PASTE THIS CODE into edX
+
+# [ ] get input for input_test variable
+input_test = input();
+
+# [ ] print "True" message if "dairy" is in the input or False message if not
+if "dairy" in input_test:
+    print("True")
+elif "dairy" not in input_test:
+    print("False")
+
+
+# [ ] print True message if "nuts" is in the input or False if not
+if "nuts" in input_test:
+    print("True")
+elif "nuts" not in input_test:
+    print("False")
+
+# [ ] Challenge: Check if "seafood" is in the input - print message
+if "seafood" in input_test:
+    print("True")
+elif "seafood" not in input_test:
+    print("False")
+
+# [ ] Challenge: Check if "chocolate" is in the input - print message
+if "chocolate" in input_test:
+    print("True")
+elif "chocolate" not in input_test:
+    print("False")
