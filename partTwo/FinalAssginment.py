6a5aa8da97ea3fe84df2f03b55fbb0245a00f194
Eimantas <eimantas.blazevicius@bazaarvoice.com>
2018-11-07 02:12:41 +0200

    FinalAssignment

--- partTwo/FinalAssginment.py
+++ partTwo/FinalAssginment.py
@@ -7,26 +7,54 @@ between user input and values that are in the file
 '''
 
 
+# read file and collect data from there
 def readafile():
-
     allValues = []
-    rFile = open('elements.txt','r')
+    rFile = open('elements.txt', 'r')
     rFile.seek(0)
     for line in rFile.readlines():
         allValues.append(line.rstrip('\n').lower())
     return allValues
 
 
+# Collect user input values
 def getuservalues():
-
     theGuesses = []
-    while True:
+    while len(theGuesses) <= 4:
         print("Guess 5 atomic elements of first 20 elements from the table: ")
-        userInput = input("Enter the name of the element: ").lower()
-        if userInput == "":
+        userinput = input("Enter the name of the element: ").lower()
+        if userinput == "":
             print("Error: No empty values are allowed")
             continue
-        elif userInput in theGuesses:
+        elif userinput in theGuesses:
             print("Error: No Duplicate values are allowed")
         else:
-            theGuesses.append(userInput)
+            theGuesses.append(userinput)
+    return theGuesses
+
+
+# function to compare lists
+def compare_answer(data_list, user_list):
+    correct = []
+    wrong = []
+    for name in user_list:
+        if name in data_list:
+            correct.append(name)
+        elif name not in data_list:
+            wrong.append(name)
+        else:
+            pass
+    # all the printing happens in this block
+    print(str(len(correct * 20)) + "% is correct!")
+    print("Found: ", end=" ")
+    print(*correct, sep=" ")
+    print("Not found: ", end=" ")
+    print(*wrong, sep=" ")
+
+
+# Actual code.. Feels good
+
+compare_answer(readafile(), getuservalues())
+
+# it was surprise for me, that functions do still do all their job, even though it is just passed as variables
+# to the third function
